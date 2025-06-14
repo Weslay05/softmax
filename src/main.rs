182769736bf182d538db6cb87dc91b56481c0a70
Weslay <weslaytchibindi3@gmail.com>
2025-06-14 22:46:31 +0200

    Optimize Softmax
    - updated to just call the softmax function once
    - softmax function now returns a vector

--- src/main.rs
+++ src/main.rs
@@ -15,40 +15,46 @@ fn main() {
         vec.push(val[i]);
         i += 1;
     }
-    /* // Push all ints from 1-5
-    let mut i:f32 = 1.0;
-    while i <= 5.0 {
-        vec.push(i);
-        i += 1.0;
-    } */
 
-    // Print Vector and Softmax Results
+    // Vector with all probabilities of vec
+    let vec_probs:Vec<f32> = softmax(vec.clone(), constant);
+
+    // Prints input and output vector
     let mut i:usize = 0;
     while i < vec.len() {
-        let softmax_result:f32 = softmax(i, vec.clone(), constant);
         print!("[{i}]{} -> ", vec[i]);
-        print!("Softmax -> {} -> ", softmax_result);
+        print!("Softmax -> {} -> ", vec_probs[i]);
         // if softmax_result < 0.10 {print!("0")};
-        print!("{:.1}%\n", softmax_result * 100.0);
+        print!("{:.1}%\n", vec_probs[i] * 100.0);
         i += 1;
     }
 }
 
 // Softmax Function with a Custom Constant besides e
-fn softmax(index:usize, vector:Vec<f32>, constant:f32) -> f32 {
-    // Create new Vector with all values after Constant^value
-    let mut constant_vec: Vec<f32> = Vec::new();
+fn softmax(ivector:Vec<f32>, iconstant:f32) -> Vec<f32> { // i stands for input
+    // New Vector for Constant^value results | rvec stands for Results Vector
+    let mut rvec:Vec<f32> = Vec::new(); 
+
+    // Calculation loop for rvec
     let mut i:usize = 0;
-    while i < vector.len() {
-        let res:f32 = f32::powf(constant, vector[i]);
-        constant_vec.push(res);
+    while i < ivector.len() {
+        let res:f32 = f32::powf(iconstant, ivector[i]);
+        rvec.push(res);
         i += 1;
     }
 
-    // Calculate the divisor for softmax
-    let divisor:f32 = constant_vec.iter().copied().sum();
+    // sum of rvec for the divisor
+    let divisor:f32 = rvec.iter().copied().sum();
 
-    // Calculate and return the Final Probabilities
-    let probability:f32 = f32::powf(constant, vector[index]) / divisor;
-    return probability;
+    // New vec for all probabilities
+    let mut prob_vec:Vec<f32> = Vec::new();
+    // Probability for each object in ivec
+    let mut i:usize = 0;
+    while i < ivector.len() {
+        let res:f32 = rvec[i] / divisor;
+        prob_vec.push(res);
+        i += 1;
+    }
+
+    return prob_vec;
 }
